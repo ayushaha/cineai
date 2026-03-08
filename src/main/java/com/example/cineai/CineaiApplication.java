ed8cefff7dea99b23f8084e6ad80d14fc84a5de5
Ayush Saklani <ayushsaklani1010@gmail.com>
2026-03-09 00:11:44 +0530

    Initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/main/java/com/example/cineai/CineaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cineai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cineai/CineaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cineai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
